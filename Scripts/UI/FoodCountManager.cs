02c5ff4c3023d8fdf9ece20cd5c96e20009e2a3f
Tony <44558154+cuilantao@users.noreply.github.com>
2021-10-19 16:30:12 -0400

    add basket

--- Scripts/UI/FoodCountManager.cs
+++ Scripts/UI/FoodCountManager.cs
@@ -5,14 +5,23 @@ using UnityEngine.UI;
 
 public class FoodCountManager : MonoBehaviour
 {   
-    public Text text;
-
     private int cheese_count;
     private int banana_count;
     private int apple_count;
 
     private int[] food_req_count = { 10, 10, 10 };
 
+
+    public int get_count(int index)
+    {
+        return food_req_count[index];
+    }
+
+    public void decrease_food_count(int index, int val)
+    {
+        food_req_count[index] -= val;
+        set_food_count();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +30,7 @@ public class FoodCountManager : MonoBehaviour
 
     public void adjust_food_count(int index, int val)
     {
-        food_req_count[index] = val;
+        food_req_count[index] = Mathf.Max(0,val);
         set_food_count();
     }
 
@@ -35,24 +44,5 @@ public class FoodCountManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (gameObject.name == "CheeseCount")
-        {
-            text.text = cheese_count.ToString();
-        }
-        else if (gameObject.name == "AppleCount")
-        {
-            text.text = apple_count.ToString();
-        }
-        else if (gameObject.name == "BananaCount")
-        {
-            text.text = banana_count.ToString();
-        }
-
-        if ((cheese_count + banana_count + apple_count) == 0)
-        {
-            // TODO, DISPLAY WINNING SCREEN
-        }
-
-
     }
 }
