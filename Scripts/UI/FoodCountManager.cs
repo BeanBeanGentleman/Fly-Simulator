7529fdea73e3e1926cf7c575067c86e1055c1e44
Tony <44558154+cuilantao@users.noreply.github.com>
2021-10-19 14:16:07 -0400

    Dev tony (#21)
    * enemy patrol
    
    * random switch between attack type
    
    * animator
    
    * new change
    
    * merged with 3c
    
    * Fixing the 3C Scene
    
    * Fixing the UI MAIN SCENE
    
    * checkingout to other branch
    
    * merge food hpbar
    
    Co-authored-by: wang_w571 <wang_w571@hotmail.com>

---
+++ Scripts/UI/FoodCountManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodCountManager : MonoBehaviour
+{   
+    public Text text;
+
+    private int cheese_count;
+    private int banana_count;
+    private int apple_count;
+
+    private int[] food_req_count = { 10, 10, 10 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        set_food_count();
+    }   
+
+    public void adjust_food_count(int index, int val)
+    {
+        food_req_count[index] = val;
+        set_food_count();
+    }
+
+    void set_food_count()
+    {
+        cheese_count = food_req_count[0];
+        banana_count = food_req_count[1];
+        apple_count = food_req_count[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.name == "CheeseCount")
+        {
+            text.text = cheese_count.ToString();
+        }
+        else if (gameObject.name == "AppleCount")
+        {
+            text.text = apple_count.ToString();
+        }
+        else if (gameObject.name == "BananaCount")
+        {
+            text.text = banana_count.ToString();
+        }
+
+        if ((cheese_count + banana_count + apple_count) == 0)
+        {
+            // TODO, DISPLAY WINNING SCREEN
+        }
+
+
+    }
+}
