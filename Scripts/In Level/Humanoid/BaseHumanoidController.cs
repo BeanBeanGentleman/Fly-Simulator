abc762e84dbd6f95acd4969effa2ec2480350c4f
wang_w571 <wang_w571@hotmail.com>
2021-11-11 01:44:42 +0800

    IKR

--- Scripts/In Level/Humanoid/BaseHumanoidController.cs
+++ Scripts/In Level/Humanoid/BaseHumanoidController.cs
@@ -1,18 +1,247 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
 
 namespace In_Level.Humanoid
 {
+	[RequireComponent(typeof(Animator))]
+	[RequireComponent(typeof(CapsuleCollider))]
+	[RequireComponent(typeof(Rigidbody))]
     public class BaseHumanoidController : MonoBehaviour
     {
-        public Animator animator;
-        public int LayerIndex;
+	    public int HandLayerIndex = 2;
         public Transform TargetTransform; // = BFC.transform
+        
+        public float animSpeed = 1.5f;				// アニメーション再生速度設定
+		public float lookSmoother = 3.0f;			// a smoothing setting for camera motion
+		public bool useCurves = true;				// Mecanimでカーブ調整を使うか設定する
+		// このスイッチが入っていないとカーブは使われない
+		public float useCurvesHeight = 0.5f;		// カーブ補正の有効高さ（地面をすり抜けやすい時には大きくする）
 
+		// 以下キャラクターコントローラ用パラメタ
+		// 前進速度
+		public float forwardSpeed = 7.0f;
+		// 後退速度
+		public float backwardSpeed = 2.0f;
+		// 旋回速度
+		public float rotateSpeed = 2.0f;
 
-        public virtual void SetSwapperPos()
+		public bool rest = false;
+		
+		
+		// キャラクターコントローラ（カプセルコライダ）の参照
+		private CapsuleCollider col;
+		private Rigidbody rb;
+
+		private NavMeshAgent nma;
+		// キャラクターコントローラ（カプセルコライダ）の移動量
+		private Vector3 velocity;
+		// CapsuleColliderで設定されているコライダのHeiht、Centerの初期値を収める変数
+		private float orgColHight;
+		private Vector3 orgVectColCenter;
+		private Animator anim;							// キャラにアタッチされるアニメーターへの参照
+		private AnimatorStateInfo currentBaseState;			// base layerで使われる、アニメーターの現在の状態の参照
+
+		private GameObject cameraObject;	// メインカメラへの参照
+		
+		// アニメーター各ステートへの参照
+		static int idleState = Animator.StringToHash ("Base Layer.Idle");
+		static int locoState = Animator.StringToHash ("Base Layer.Locomotion");
+		static int jumpState = Animator.StringToHash ("Base Layer.Jump");
+		static int restState = Animator.StringToHash ("Base Layer.Rest");
+
+		public bool SWAP;
+
+		public float SwapProgress;
+		
+		// 初期化
+		void Start ()
+		{
+			// Animatorコンポーネントを取得する
+			anim = GetComponent<Animator> ();
+			// CapsuleColliderコンポーネントを取得する（カプセル型コリジョン）
+			col = GetComponent<CapsuleCollider> ();
+			rb = GetComponent<Rigidbody> ();
+			nma = GetComponent<NavMeshAgent>();
+			//メインカメラを取得する
+			cameraObject = GameObject.FindWithTag ("MainCamera");
+			// CapsuleColliderコンポーネントのHeight、Centerの初期値を保存する
+			orgColHight = col.height;
+			orgVectColCenter = col.center;
+			anim.speed = 1;
+		}
+	
+	
+		// 以下、メイン処理.リジッドボディと絡めるので、FixedUpdate内で処理を行う.
+		void FixedUpdate ()
+		{
+			// this.rb.velocity = Vector3.forward * 2;
+			float h = 0;
+			float v = this.nma.velocity.magnitude;
+			anim.SetFloat ("Speed", v);							// Animator側で設定している"Speed"パラメタにvを渡す
+			anim.SetFloat ("Direction", h); 						// Animator側で設定している"Direction"パラメタにhを渡す
+			anim.speed = v!=0?(Mathf.Lerp(anim.speed, animSpeed, 0.02f)):1;								// Animatorのモーション再生速度に animSpeedを設定する
+			currentBaseState = anim.GetCurrentAnimatorStateInfo (0);	// 参照用のステート変数にBase Layer (0)の現在のステートを設定する
+			rb.useGravity = true;//ジャンプ中に重力を切るので、それ以外は重力の影響を受けるようにする
+		
+		
+		
+			// 以下、キャラクターの移動処理
+			velocity = new Vector3 (0, 0, v);		// 上下のキー入力からZ軸方向の移動量を取得
+			// キャラクターのローカル空間での方向に変換
+			velocity = transform.TransformDirection (velocity);
+			//以下のvの閾値は、Mecanim側のトランジションと一緒に調整する
+			if (v > 0.1) {
+				velocity *= forwardSpeed;		// 移動速度を掛ける
+			} else if (v < -0.1) {
+				velocity *= backwardSpeed;	// 移動速度を掛ける
+			}
+
+			// 上下のキー入力でキャラクターを移動させる
+			// transform.localPosition += velocity * Time.fixedDeltaTime;
+
+			// 左右のキー入力でキャラクタをY軸で旋回させる
+			transform.Rotate (0, h * rotateSpeed, 0);	
+	
+
+			// 以下、Animatorの各ステート中での処理
+			// Locomotion中
+			// 現在のベースレイヤーがlocoStateの時
+			if (currentBaseState.nameHash == locoState) {
+				//カーブでコライダ調整をしている時は、念のためにリセットする
+				if (useCurves) {
+					resetCollider ();
+				}
+			}
+		// JUMP中の処理
+		// 現在のベースレイヤーがjumpStateの時
+		else if (currentBaseState.nameHash == jumpState) {
+				cameraObject.SendMessage ("setCameraPositionJumpView");	// ジャンプ中のカメラに変更
+				// ステートがトランジション中でない場合
+				if (!anim.IsInTransition (0)) {
+				
+					// 以下、カーブ調整をする場合の処理
+					if (useCurves) {
+						// 以下JUMP00アニメーションについているカーブJumpHeightとGravityControl
+						// JumpHeight:JUMP00でのジャンプの高さ（0〜1）
+						// GravityControl:1⇒ジャンプ中（重力無効）、0⇒重力有効
+						float jumpHeight = anim.GetFloat ("JumpHeight");
+						float gravityControl = anim.GetFloat ("GravityControl"); 
+						if (gravityControl > 0)
+							rb.useGravity = false;	//ジャンプ中の重力の影響を切る
+										
+						// レイキャストをキャラクターのセンターから落とす
+						Ray ray = new Ray (transform.position + Vector3.up, -Vector3.up);
+						RaycastHit hitInfo = new RaycastHit ();
+						// 高さが useCurvesHeight 以上ある時のみ、コライダーの高さと中心をJUMP00アニメーションについているカーブで調整する
+						if (Physics.Raycast (ray, out hitInfo)) {
+							if (hitInfo.distance > useCurvesHeight) {
+								col.height = orgColHight - jumpHeight;			// 調整されたコライダーの高さ
+								float adjCenterY = orgVectColCenter.y + jumpHeight;
+								col.center = new Vector3 (0, adjCenterY, 0);	// 調整されたコライダーのセンター
+							} else {
+								// 閾値よりも低い時には初期値に戻す（念のため）					
+								resetCollider ();
+							}
+						}
+					}
+					// Jump bool値をリセットする（ループしないようにする）				
+					anim.SetBool ("Jump", false);
+				}
+			}
+		// IDLE中の処理
+		// 現在のベースレイヤーがidleStateの時
+		else if (currentBaseState.nameHash == idleState) {
+				//カーブでコライダ調整をしている時は、念のためにリセットする
+				if (useCurves) {
+					resetCollider ();
+				}
+				// スペースキーを入力したらRest状態になる
+				if (rest) {
+					anim.SetBool ("Rest", true);
+				}
+			}
+		// REST中の処理
+		// 現在のベースレイヤーがrestStateの時
+		else if (currentBaseState.nameHash == restState) {
+				//cameraObject.SendMessage("setCameraPositionFrontView");		// カメラを正面に切り替える
+				// ステートが遷移中でない場合、Rest bool値をリセットする（ループしないようにする）
+				if (!anim.IsInTransition (0)) {
+					anim.SetBool ("Rest", false);
+				}
+			}
+
+			SWAP = (TargetTransform.position - this.transform.position).sqrMagnitude < 400;
+			if (SWAP)
+			{
+				SwapProgress = Mathf.Clamp01(SwapProgress + (animSpeed * Time.fixedDeltaTime));
+			}
+			else
+			{
+				SwapProgress = Mathf.Clamp01(SwapProgress - (animSpeed * Time.fixedDeltaTime));
+			}
+			this.nma.SetDestination(TargetTransform.position);
+			
+
+		}
+		
+
+		// キャラクターのコライダーサイズのリセット関数
+		void resetCollider ()
+		{
+			// コンポーネントのHeight、Centerの初期値を戻す
+			col.height = orgColHight;
+			col.center = orgVectColCenter;
+		}
+
+		private void OnAnimatorIK(int layerIndex)
+		{
+			if(layerIndex != HandLayerIndex) return;
+			if (SWAP)
+			{
+				if (SwapProgress < 0.5f)
+				{
+					SetSwapperPreparationPos();
+				}
+				else
+				{
+					SetSwapperPos();
+				}
+			}
+			else
+			{
+				SetSwapperPosBack();
+			}
+		}
+		
+		public virtual void SetSwapperPreparationPos()
+		{
+			anim.SetBool("Swapping", true);
+			anim.SetIKPosition(AvatarIKGoal.RightHand, this.transform.position + Vector3.up * 30);
+			// anim.SetIKRotation(AvatarIKGoal.RightHand, TargetTransform.transform.rotation); TODO
+			// anim.SetIKPositionWeight(AvatarIKGoal.RightHand, Mathf.Lerp(anim.GetIKPositionWeight(AvatarIKGoal.RightHand), 0.5f, 0.02f));
+			anim.SetIKPositionWeight(AvatarIKGoal.RightHand, SwapProgress);
+			// anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+			
+		}
+		public virtual void SetSwapperPos()
         {
-            animator.SetIKPosition(AvatarIKGoal.RightHand, TargetTransform.transform.position);
-            animator.SetIKRotation(AvatarIKGoal.RightHand, TargetTransform.transform.rotation);
+	        anim.SetBool("Swapping", true);
+            anim.SetIKPosition(AvatarIKGoal.RightHand, Vector3.Lerp(this.transform.position + Vector3.up * 100, TargetTransform.transform.position,
+	            (SwapProgress - 0.5f)*2));
+            // anim.SetIKRotation(AvatarIKGoal.RightHand, TargetTransform.transform.rotation); TODO
+            // anim.SetIKPositionWeight(AvatarIKGoal.RightHand, Mathf.Lerp(anim.GetIKPositionWeight(AvatarIKGoal.RightHand), 1, 0.02f));
+            anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 0.5f);
+            // anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+            
         }
+		public virtual void SetSwapperPosBack()
+		{
+			anim.SetBool("Swapping", false);
+			anim.SetIKPositionWeight(AvatarIKGoal.RightHand, Mathf.Lerp(anim.GetIKPositionWeight(AvatarIKGoal.RightHand), 0, 0.02f));
+			anim.SetIKRotationWeight(AvatarIKGoal.RightHand, Mathf.Lerp(anim.GetIKRotationWeight(AvatarIKGoal.RightHand), 0, 0.02f));
+			
+		}
     }
 }
