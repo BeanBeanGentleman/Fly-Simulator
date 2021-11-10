a1d0b08df6f6b279771d2977d94fe83463d3355b
wang_w571 <wang_w571@hotmail.com>
2021-11-10 18:18:44 +0800

    Starting IK Humanoid

---
+++ Scripts/In Level/Humanoid/BaseHumanoidController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace In_Level.Humanoid
+{
+    public class BaseHumanoidController : MonoBehaviour
+    {
+        public Animator animator;
+        public int LayerIndex;
+        public Transform TargetTransform; // = BFC.transform
+
+
+        public virtual void SetSwapperPos()
+        {
+            animator.SetIKPosition(AvatarIKGoal.RightHand, TargetTransform.transform.position);
+            animator.SetIKRotation(AvatarIKGoal.RightHand, TargetTransform.transform.rotation);
+        }
+    }
+}
