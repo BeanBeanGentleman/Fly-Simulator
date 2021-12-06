f9a45cbfcda6b0908a48b5a97dfbfabee2fe7d85
Tony <44558154+cuilantao@users.noreply.github.com>
2021-12-05 19:03:29 -0500

    s

--- Scripts/In Level/Fly/Fly Control/FlyInput.cs
+++ Scripts/In Level/Fly/Fly Control/FlyInput.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Fly-Simulator/Scripts/In Level/Fly/Fly Control/FlyInput.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/In Level/Fly/Fly Control/FlyInput.inputactions'
 
 using System;
 using System.Collections;
